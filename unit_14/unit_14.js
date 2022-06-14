ef4b79e5f33440d79d174c2a30899acaf05aaf1f
master <vdukhovnik1@gmail.com>
2022-06-14 23:29:33 +0200

    14.06

--- unit_14/unit_14.js
+++ unit_14/unit_14.js
@@ -23,6 +23,15 @@ function showWeather(data) {
     console.log(data);
     document.getElementById('city-name').innerHTML = data.name
     document.getElementById('degrees').innerHTML = Math.floor(data.main.temp) + '°'
+    document.getElementById('clouds').innerHTML = data.weather[0].main
+    document.getElementById('country').innerHTML = data.sys.country
+
+    if (data.weather[0].main === 'Clouds') {
+        document.querySelector('img').src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw4rywwXRwFoqoHRBQmFLyRKWk5MOMFwzJFBfRqlHcqecOZQnsCRVdGdVIdageCYQc5Co&usqp=CAU'
+    }
+    if (data.weather[0].main === 'Clear') {
+        document.querySelector('img').src = 'https://media.istockphoto.com/vectors/cute-flat-sun-icon-vector-id1124567572?k=20&m=1124567572&s=612x612&w=0&h=qA2-ugQviG9uGvpn5-K90sK9w5QZjd3TetULc_5VECc='
+    }
 }
 
 getWeather();
